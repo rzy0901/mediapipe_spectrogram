285b940ee5333bef6d118bc720da0a5867eddf93
Ren Zhenyu <2391928709@qq.com>
2023-10-20 20:59:49 +0800

    Update test.m

--- test.m
+++ test.m
@@ -1,9 +1,9 @@
 % https://zhuanlan.zhihu.com/p/422696920
 clc; clear; close all;
-Tx_pos = [5 5 5];
-Rx_pos = [-5 5 5];
-joint1 = [1 2 1];
-joint2 = [2 3 4];
+Tx_pos = [2 2 1.5];
+Rx_pos = [-2 1 2];
+joint1 = [-1 -2 -1];
+joint2 = [1 2 1];
 mid = 0.5*(joint1 + joint2);
 aspect = joint1 - joint2;
 % 计算经过mid的垂直于aspect的平面P
@@ -21,23 +21,38 @@ dot(Tx_projection-mid,normal_vector)
 dot(normal_vector,Tx_projection)-dot(normal_vector,mid)
 
 %%
-scatter3(Tx_pos(1), Tx_pos(2), Tx_pos(3), 100, 'r', 'filled');
+scatter3(Tx_pos(1), Tx_pos(2), Tx_pos(3), 100, 'r', '*');
 hold on;
-scatter3(Rx_pos(1), Rx_pos(2), Rx_pos(3), 100, 'b', 'filled');
+scatter3(Rx_pos(1), Rx_pos(2), Rx_pos(3), 100, 'b', '*');
 scatter3(mid(1), mid(2), mid(3), 100, 'g', 'filled');
-[x_plane, y_plane] = meshgrid(-10:10, -10:10);
+[x_plane, y_plane] = meshgrid(-3:3, -3:3);
 % Ax + By + Cz + D = 0;
 A = normal_vector(1); B = normal_vector(2); C = normal_vector(3);
 D = - dot(normal_vector,mid);
 z_plane = (-A * x_plane - B * y_plane - D) / C;
 surf(x_plane, y_plane, z_plane, 'FaceAlpha', 0.3, 'FaceColor', 'c', 'EdgeColor', 'none');
-scatter3(Tx_projection(1), Tx_projection(2), Tx_projection(3), 100, 'm', 'filled');
-scatter3(Rx_projection(1), Rx_projection(2), Rx_projection(3), 100, 'y', 'filled');
+scatter3(Tx_projection(1), Tx_projection(2), Tx_projection(3), 100, 'r', 'filled');
+scatter3(Rx_projection(1), Rx_projection(2), Rx_projection(3), 100, 'b', 'filled');
+line([Tx_pos(1) mid(1)],[Tx_pos(2) mid(2)],...
+    [Tx_pos(3) mid(3)],'LineStyle',':',...
+    'color','k','LineWidth',2)
+line([Tx_projection(1) mid(1)],[Tx_projection(2) mid(2)],...
+    [Tx_projection(3) mid(3)],'LineStyle',':',...
+    'color','k','LineWidth',2)
+line([Rx_pos(1) mid(1)],[Rx_pos(2) mid(2)],...
+    [Rx_pos(3) mid(3)],'LineStyle',':',...
+    'color','k','LineWidth',2)
+line([Rx_projection(1) mid(1)],[Rx_projection(2) mid(2)],...
+    [Rx_projection(3) mid(3)],'LineStyle',':',...
+    'color','k','LineWidth',2)
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
 legend('Tx\_pos', 'Rx\_pos', 'mid', 'Plane P', 'Tx\_projection', 'Rx\_projection');
-text(mid(1), mid(2), mid(3), ['\angle = ', num2str(angle_rad, '%.2f')], 'Color', 'k', 'FontSize', 12);
+title(['\angle (rad) = ', num2str(angle_rad, '%.2f')]);
+% text(mid(1), mid(2), mid(3), ['\angle = ', num2str(angle_rad, '%.2f')], 'Color', 'k', 'FontSize', 12);
 grid on;
 axis equal;
 hold off;
+zlim([-3 3])
+view(-10,80)
